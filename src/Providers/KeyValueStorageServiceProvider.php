404c5cd5b484b57ade7e95723062695099fcf307
PasanBhanu <pasanbguruge@hotmail.com>
2020-03-22 10:10:17 +0530

    Initial commit

---
+++ src/Providers/KeyValueStorageServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SoftinkLab\LaravelKeyvalueStorage\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class KeyValueStorageServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+
+    public function register()
+    {
+
+    }
+}
